0651d637235d5e88a2a01cab31c015b972991b32
puyuyu1234 <145762224+puyuyu1234@users.noreply.github.com>
2024-09-02 18:01:25 +0900

    Fontコマンド追加

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 "use strict";
 
+function getLen(str) {
+    var result = 0;
+    for (var i = 0; i < str.length; i++) {
+        var chr = str.charCodeAt(i);
+        if (
+            (chr >= 0x00 && chr < 0x81) ||
+            chr === 0xf8f0 ||
+            (chr >= 0xff61 && chr < 0xffa0) ||
+            (chr >= 0xf8f1 && chr < 0xf8f4)
+        ) {
+            //半角文字の場合は1を加算
+            result += 1;
+        } else {
+            //それ以外の文字の場合は2を加算
+            result += 2;
+        }
+    }
+    //結果を返す
+    return result;
+}
+
 const context = canvas.getContext("2d");
-context.fillStyle = "white";
 
 const images = {};
 for (let i = 0; i <= 6; i++) {
@@ -14,16 +34,107 @@ for (let i = 0; i <= 6; i++) {
 
 let waitingEvent = () => {};
 const render = () => {
-    context.font = `${fontSize.value}px monospace`;
+    const defaultFont = {
+        fontSize: +fontSize.value,
+        positionX: +positionX.value,
+        positionY: +positionY.value,
+        color: "white",
+    };
+    context.font = `${defaultFont.fontSize}px monospace`;
+    context.fillStyle = defaultFont.color;
     waitingEvent = () => {
         context.drawImage(images[bg.value], 0, 0);
 
         const textArray = text.value.split("\n");
+        let currentFontData = {
+            fontSize: defaultFont.fontSize,
+            bold: false,
+            italic: false,
+            x: 0,
+            y: 0,
+        };
+        const generateFont = () => {
+            let s = "";
+            if (currentFontData.bold) {
+                s += "bold ";
+            }
+            if (currentFontData.italic) {
+                s += "italic ";
+            }
+            s += `${currentFontData.fontSize}px monospace`;
+
+            return s;
+        };
         for (const id in textArray) {
             const str = textArray[id];
-            const x = +positionX.value;
-            const y = +positionY.value + id * fontSize.value;
-            context.fillText(str, x, y);
+            const stringParts = [];
+            const regex = /\[font [^\]]+\]/g;
+            let match;
+            let lastIndex = 0;
+
+            while ((match = regex.exec(str)) !== null) {
+                const startIndex = match.index;
+                const endIndex = match.index + match[0].length;
+                stringParts.push(str.slice(lastIndex, startIndex));
+                stringParts.push(str.slice(startIndex, endIndex));
+                lastIndex = regex.lastIndex;
+            }
+            if (lastIndex < str.length) {
+                stringParts.push(str.slice(lastIndex));
+            }
+
+            let x = defaultFont.positionX;
+            let y = defaultFont.positionY + id * defaultFont.fontSize;
+            for (const part of stringParts) {
+                if (regex.exec(part)) {
+                    // font処理
+                    const fontArray = part.slice(0, -1).split(" ");
+                    for (let i = 1; i < fontArray.length; i++) {
+                        const eqArray = fontArray[i].split("=");
+                        switch (eqArray[0]) {
+                            case "size":
+                                currentFontData.fontSize = eqArray[1];
+                                context.font = generateFont();
+                                break;
+                            case "color":
+                                context.fillStyle = eqArray[1];
+                                break;
+                            case "italic":
+                                currentFontData.italic = true;
+                                context.font = generateFont();
+                                break;
+                            case "bold":
+                                currentFontData.bold = true;
+                                context.font = generateFont();
+                                break;
+                            case "x":
+                                currentFontData.x = +eqArray[1];
+                                break;
+                            case "y":
+                                currentFontData.y = +eqArray[1];
+                                break;
+                            case "reset":
+                                currentFontData.fontSize = defaultFont.fontSize;
+                                currentFontData.italic = false;
+                                currentFontData.bold = false;
+                                currentFontData.x = 0;
+                                currentFontData.y = 0;
+                                context.font = generateFont();
+                                context.fillStyle = defaultFont.color;
+                                break;
+                        }
+                    }
+                    continue;
+                }
+
+                // 文章通常処理
+                context.fillText(
+                    part,
+                    x + currentFontData.x,
+                    y + currentFontData.y
+                );
+                x += (getLen(part) * currentFontData.fontSize) / 2;
+            }
         }
     };
     // 読み込みが完了している場合は即座描画・待機中イベント消去
